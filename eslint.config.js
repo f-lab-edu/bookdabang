9046f5e4a1321b44add60a2a4e41fa4fd0f6ac3a
2scent <ad7549@gmail.com>
2025-01-12 14:38:07 +0900

    chore: eslint 세팅

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier', 'plugin:tailwindcss/recommended'),
+  {
+    rules: {
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'warn',
+      'tailwindcss/no-contradicting-classname': 'error',
+    },
+  },
+];
+
+export default eslintConfig;
