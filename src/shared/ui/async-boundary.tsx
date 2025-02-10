7c94783a968100de60a22fbb0d86d8e9a75e6688
2scent <ad7549@gmail.com>
2025-02-10 23:46:18 +0900

    feat: AsyncBoundary 기본 폴백 설정

--- src/shared/ui/async-boundary.tsx
+++ src/shared/ui/async-boundary.tsx
@@ -2,13 +2,25 @@
 
 import { Suspense, SuspenseProps } from 'react';
 import { ErrorBoundary, ErrorBoundaryPropsWithRender } from 'react-error-boundary';
+import { Loading } from './loading';
+import { Rejected } from './rejected';
 
 export interface AsyncBoundaryProps extends Omit<ErrorBoundaryPropsWithRender, 'fallbackRender'> {
-  pendingFallback: SuspenseProps['fallback'];
-  rejectedFallback: ErrorBoundaryPropsWithRender['fallbackRender'];
+  pendingFallback?: SuspenseProps['fallback'];
+  rejectedFallback?: ErrorBoundaryPropsWithRender['fallbackRender'];
 }
 
-const AsyncBoundary = ({ pendingFallback, rejectedFallback, children, ...errorBoundaryProps }: AsyncBoundaryProps) => {
+const AsyncBoundary = ({
+  pendingFallback = <Loading />,
+  rejectedFallback = ({ error, resetErrorBoundary }) => (
+    <Rejected
+      error={error}
+      onReset={resetErrorBoundary}
+    />
+  ),
+  children,
+  ...errorBoundaryProps
+}: AsyncBoundaryProps) => {
   return (
     <ErrorBoundary
       fallbackRender={rejectedFallback}
