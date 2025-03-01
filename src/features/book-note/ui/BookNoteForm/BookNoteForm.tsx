3bc858ec0a107e6614df5f3b0591aa218c4006a0
2scent <ad7549@gmail.com>
2025-03-01 22:53:26 +0900

    feat: 다음 스텝으로 이동할 때 유효성 검사

--- src/features/book-note/ui/BookNoteForm/BookNoteForm.tsx
+++ src/features/book-note/ui/BookNoteForm/BookNoteForm.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { useParams } from 'next/navigation';
+import { FormProvider } from 'react-hook-form';
 import { useFormFunnel } from '@/shared/lib/form';
 import { RenderCase } from '@/shared/ui/render-case';
 import { useBookDetail } from '@/entities/book';
+import { BookNoteFormValues } from '../../model/book-note-form-values';
 import BookNoteFormActions from './BookNoteFormActions';
 import ReadingInfoStep from './step/ReadingInfoStep';
 import RatingStep from './step/RatingStep';
@@ -11,35 +13,51 @@ import ReviewStep from './step/ReviewStep';
 import QuotesStep from './step/QuotesStep';
 import VisibilityStep from './step/VisibilityStep';
 
+const triggerFields: Record<number, (keyof BookNoteFormValues)[]> = {
+  1: ['readingStatus', 'startDate', 'endDate'],
+  2: ['recommended', 'overallRating'],
+  3: ['content'],
+  4: ['quotes'],
+  5: ['visibility'],
+};
+
 export default function BookNoteForm() {
   const { isbn } = useParams<{ isbn: string }>()!;
 
   const book = useBookDetail(isbn);
 
-  const { navigation } = useFormFunnel({
+  const {
+    form,
+    navigation: { currentStep, isFirstStep, isLastStep, goToPreviousStep, goToNextStep },
+  } = useFormFunnel<BookNoteFormValues>({
     totalSteps: 5,
   });
 
-  const { currentStep, isFirstStep, isLastStep, goToPreviousStep, goToNextStep } = navigation;
+  const handleNext = async () => {
+    const isValid = await form.trigger(triggerFields[currentStep]);
+    if (isValid) goToNextStep();
+  };
 
   return (
-    <form className="mx-auto w-full max-w-4xl space-y-8 p-4 md:p-6">
-      <RenderCase
-        value={currentStep}
-        cases={{
-          1: <ReadingInfoStep book={book} />,
-          2: <RatingStep />,
-          3: <ReviewStep />,
-          4: <QuotesStep />,
-          5: <VisibilityStep />,
-        }}
-      />
-      <BookNoteFormActions
-        previousDisabled={isFirstStep}
-        nextDisabled={isLastStep}
-        onPrevious={goToPreviousStep}
-        onNext={goToNextStep}
-      />
-    </form>
+    <FormProvider {...form}>
+      <form className="mx-auto w-full max-w-4xl space-y-8 p-4 md:p-6">
+        <RenderCase
+          value={currentStep}
+          cases={{
+            1: <ReadingInfoStep book={book} />,
+            2: <RatingStep />,
+            3: <ReviewStep />,
+            4: <QuotesStep />,
+            5: <VisibilityStep />,
+          }}
+        />
+        <BookNoteFormActions
+          previousDisabled={isFirstStep}
+          nextDisabled={isLastStep}
+          onPrevious={goToPreviousStep}
+          onNext={handleNext}
+        />
+      </form>
+    </FormProvider>
   );
 }
