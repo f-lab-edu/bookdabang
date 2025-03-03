1459d03acca7fc50e4e11c45ce7d028abc5e7414
2scent <ad7549@gmail.com>
2025-03-03 18:48:45 +0900

    feat: 독후감 스텝의 필드 rhf로 제어

--- src/features/book-note/ui/BookNoteForm/step/ReviewStep.tsx
+++ src/features/book-note/ui/BookNoteForm/step/ReviewStep.tsx
@@ -1,7 +1,14 @@
+import { useFormContext } from 'react-hook-form';
 import { Card, CardHeader, CardTitle, CardContent } from '@/shared/ui/card';
 import { Textarea } from '@/shared/ui/textarea';
+import { BookNoteFormValues } from '../../../model/book-note-form-values';
 
 export default function ReviewStep() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<BookNoteFormValues>();
+
   return (
     <Card>
       <CardHeader>
@@ -9,11 +16,12 @@ export default function ReviewStep() {
       </CardHeader>
       <CardContent>
         <Textarea
+          {...register('content', { required: true })}
           id="content"
-          name="content"
           className="min-h-[200px]"
           placeholder="독후감을 작성해 보세요!"
         />
+        {errors.content && <p className="text-red-500">독후감은 필수 값입니다.</p>}
       </CardContent>
     </Card>
   );
