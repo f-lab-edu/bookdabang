8f683034392c0d2ea708dc8a6a3df82c2ac72df4
2scent <ad7549@gmail.com>
2025-03-03 23:28:41 +0900

    feat: 공개 설정 스텝의 필드 rhf로 제어

--- src/features/book-note/ui/BookNoteForm/step/VisibilityStep.tsx
+++ src/features/book-note/ui/BookNoteForm/step/VisibilityStep.tsx
@@ -1,8 +1,12 @@
+import { Controller, useFormContext } from 'react-hook-form';
 import { Card, CardHeader, CardTitle, CardContent } from '@/shared/ui/card';
 import { Label } from '@/shared/ui/label';
 import { Switch } from '@/shared/ui/switch';
+import { BookNoteFormValues } from '../../../model/book-note-form-values';
 
 export default function VisibilityStep() {
+  const { control } = useFormContext<BookNoteFormValues>();
+
   return (
     <Card>
       <CardHeader>
@@ -10,10 +14,17 @@ export default function VisibilityStep() {
       </CardHeader>
       <CardContent>
         <div className="flex items-center space-x-2">
-          <Switch
-            id="visibility"
-            checked
-          />
+          <Controller
+            control={control}
+            name="visibility"
+            render={({ field }) => (
+              <Switch
+                id="visibility"
+                checked={field.value}
+                onCheckedChange={field.onChange}
+              />
+            )}
+          ></Controller>
           <Label htmlFor="visibility">전체 공개</Label>
         </div>
       </CardContent>
