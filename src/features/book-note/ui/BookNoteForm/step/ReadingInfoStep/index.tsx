5f1e5e0c834edecfb3921a19c884dac07b5c7c73
2scent <ad7549@gmail.com>
2025-03-08 00:45:44 +0900

    refactor: DatePicker 컴포넌트 분리

--- src/features/book-note/ui/BookNoteForm/step/ReadingInfoStep/index.tsx
+++ src/features/book-note/ui/BookNoteForm/step/ReadingInfoStep/index.tsx
@@ -1,13 +1,7 @@
-import { format } from 'date-fns';
-import { ko } from 'date-fns/locale';
-import { CalendarIcon } from 'lucide-react';
 import { Controller, useFormContext } from 'react-hook-form';
-import { cn } from '@/shared/lib/utils';
-import { Button } from '@/shared/ui/button';
-import { Calendar } from '@/shared/ui/calendar';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/shared/ui/card';
+import { DatePicker } from '@/shared/ui/date-picker';
 import { Label } from '@/shared/ui/label';
-import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/popover';
 import { BookDetail } from '@/entities/book';
 import { BookNoteFormValues } from '../../../../model/book-note-form-values';
 import BookInfo from './BookInfo';
@@ -34,71 +28,33 @@ export default function ReadingInfoStep({ book }: ReadingInfoStepProps) {
         <ReadingInfoSelect />
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
           <div className="space-y-2">
+            <Label htmlFor="startDate">읽기 시작한 날짜</Label>
             <Controller
               control={control}
               name="startDate"
               rules={{ required: true }}
               render={({ field }) => (
                 <>
-                  <Label htmlFor="startDate">읽기 시작한 날짜</Label>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant="outline"
-                        className={cn(
-                          'w-full justify-start text-left font-normal',
-                          !field.value && 'text-muted-foreground',
-                        )}
-                      >
-                        <CalendarIcon className="mr-2 size-4" />
-                        {field.value ? format(field.value, 'PPP', { locale: ko }) : <span>날짜 선택</span>}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                      <Calendar
-                        mode="single"
-                        initialFocus
-                        selected={field.value}
-                        onSelect={field.onChange}
-                      />
-                    </PopoverContent>
-                  </Popover>
+                  <DatePicker
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
                 </>
               )}
             ></Controller>
             {errors.startDate && <p className="text-red-500">읽기 시작한 날짜는 필수 값입니다.</p>}
           </div>
           <div className="space-y-2">
+            <Label htmlFor="endDate">읽은 마지막 날짜</Label>
             <Controller
               control={control}
               name="endDate"
               rules={{ required: true }}
               render={({ field }) => (
-                <>
-                  <Label htmlFor="endDate">읽은 마지막 날짜</Label>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant="outline"
-                        className={cn(
-                          'w-full justify-start text-left font-normal',
-                          !field.value && 'text-muted-foreground',
-                        )}
-                      >
-                        <CalendarIcon className="mr-2 size-4" />
-                        {field.value ? format(field.value, 'PPP', { locale: ko }) : <span>날짜 선택</span>}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                      <Calendar
-                        mode="single"
-                        initialFocus
-                        selected={field.value}
-                        onSelect={field.onChange}
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </>
+                <DatePicker
+                  value={field.value}
+                  onChange={field.onChange}
+                />
               )}
             ></Controller>
             {errors.endDate && <p className="text-red-500">읽은 마지막 날짜는 필수 값입니다.</p>}
