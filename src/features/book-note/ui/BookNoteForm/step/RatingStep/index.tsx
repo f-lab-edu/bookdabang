a7a6d3e25fa1f146548f29cdb87214e01d4dca2d
2scent <ad7549@gmail.com>
2025-03-08 01:00:09 +0900

    refactor: RatingField 컴포넌트 분리

--- src/features/book-note/ui/BookNoteForm/step/RatingStep/index.tsx
+++ src/features/book-note/ui/BookNoteForm/step/RatingStep/index.tsx
@@ -1,16 +1,8 @@
-import { Controller, useFormContext } from 'react-hook-form';
 import { Card, CardHeader, CardTitle, CardContent } from '@/shared/ui/card';
-import { Label } from '@/shared/ui/label';
-import { BookNoteFormValues } from '../../../../model/book-note-form-values';
-import StarRating from '../../StarRating';
 import RecommendField from './RecommendField';
+import RatingField from './RatingField';
 
 export default function RatingStep() {
-  const {
-    control,
-    formState: { errors },
-  } = useFormContext<BookNoteFormValues>();
-
   return (
     <Card>
       <CardHeader>
@@ -18,21 +10,7 @@ export default function RatingStep() {
       </CardHeader>
       <CardContent className="space-y-4">
         <RecommendField />
-        <div className="space-y-2">
-          <Label>전체 평점</Label>
-          <Controller
-            control={control}
-            name="overallRating"
-            rules={{ required: true }}
-            render={({ field }) => (
-              <StarRating
-                rating={field.value}
-                onRatingChange={field.onChange}
-              />
-            )}
-          ></Controller>
-          {errors.overallRating && <p className="text-red-500">전체 평점은 필수 값입니다.</p>}
-        </div>
+        <RatingField />
       </CardContent>
     </Card>
   );
