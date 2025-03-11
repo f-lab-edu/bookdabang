c03cb4a5f302dd7c0054f97e70041d569b248ed7
2scent <ad7549@gmail.com>
2025-03-11 20:32:05 +0900

    feat: 마지막 스텝에서 유효성 검사 실패 시 실패한 첫 번째 필드로 포커스

--- src/features/book-note/ui/BookNoteForm/BookNoteForm.tsx
+++ src/features/book-note/ui/BookNoteForm/BookNoteForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import { useForm } from 'react-hook-form';
+import { FieldErrors, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FunnelProvider, useFunnel } from '@/shared/lib/funnel';
 import { Form } from '@/shared/ui/form';
@@ -14,6 +14,15 @@ import RatingStep from './step/RatingStep';
 import ReviewStep from './step/ReviewStep';
 import QuotesStep from './step/QuotesStep';
 import VisibilityStep from './step/VisibilityStep';
+import { isNotNil } from 'es-toolkit';
+
+const triggerFields = new Map<number, (keyof BookNoteFormSchema)[]>([
+  [1, ['readingInfo']],
+  [2, ['recommended', 'overallRating']],
+  [3, ['content']],
+  [4, ['quotes']],
+  [5, ['visibility']],
+]);
 
 export default function BookNoteForm() {
   const { isbn } = useParams<{ isbn: string }>()!;
@@ -26,16 +35,32 @@ export default function BookNoteForm() {
 
   const funnel = useFunnel({ totalSteps: 5 });
 
-  const handleSubmit = form.handleSubmit((data) => {
+  const onSubmit = (data: BookNoteFormSchema) => {
     console.log(data);
-  });
+  };
+
+  const onError = (errors: FieldErrors<BookNoteFormSchema>) => {
+    for (const [step, fields] of triggerFields.entries()) {
+      for (const field of fields) {
+        if (isNotNil(errors[field])) {
+          funnel.goToStep(step);
+
+          setTimeout(() => {
+            form.trigger(triggerFields.get(step));
+          }, 500);
+
+          return;
+        }
+      }
+    }
+  };
 
   return (
     <FunnelProvider funnel={funnel}>
       <Form {...form}>
         <form
           className="mx-auto w-full max-w-4xl space-y-8 p-4 md:p-6"
-          onSubmit={handleSubmit}
+          onSubmit={form.handleSubmit(onSubmit, onError)}
           noValidate
         >
           <RenderCase
