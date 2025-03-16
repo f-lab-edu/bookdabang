bde4d11bb119d8418ef3a6ff7c47dc2b920588c4
2scent <ad7549@gmail.com>
2025-03-16 22:46:29 +0900

    feat: RecommendField 포커스 적용

--- src/features/book-note/ui/BookNoteForm/step/RatingStep/RecommendField.tsx
+++ src/features/book-note/ui/BookNoteForm/step/RatingStep/RecommendField.tsx
@@ -1,9 +1,9 @@
-import { isNil, isNotNil } from 'es-toolkit';
+import { isNotNil } from 'es-toolkit';
 import { Controller, useFormContext } from 'react-hook-form';
-import { ThumbsDown, ThumbsUp } from 'lucide-react';
 import { cn } from '@/shared/lib/utils';
+import { ThumbsUp, ThumbsDown } from 'lucide-react';
+import { Button } from '@/shared/ui/button';
 import { Label } from '@/shared/ui/label';
-import { RadioGroup, RadioGroupItem } from '@/shared/ui/radio-group';
 import { BookNoteFormSchema } from '../../../../model/book-note-form-schema';
 
 export default function RecommendField() {
@@ -13,52 +13,39 @@ export default function RecommendField() {
   } = useFormContext<BookNoteFormSchema>();
 
   return (
-    <div className="space-y-3">
+    <div className="space-y-2">
       <Label required>이 책을 추천할까요?</Label>
       <Controller
         control={control}
         name="recommended"
         render={({ field }) => (
-          <RadioGroup
-            className="flex"
-            value={isNil(field.value) ? undefined : field.value.toString()}
-            onValueChange={(value) => field.onChange(value === 'true')}
-          >
-            <div>
-              <RadioGroupItem
-                value="true"
-                id="recommend-true"
-                className="peer sr-only"
-              />
-              <Label
-                htmlFor="recommend-true"
-                className={cn(
-                  'flex cursor-pointer items-center rounded-md border px-4 py-2 peer-data-[state=checked]:bg-primary peer-data-[state=checked]:text-primary-foreground',
-                  'hover:bg-muted',
-                )}
-              >
-                <ThumbsUp className="mr-2 size-5" />
-                추천
-              </Label>
-            </div>
-            <div>
-              <RadioGroupItem
-                value="false"
-                id="recommend-false"
-                className="peer sr-only"
-              />
-              <Label
-                htmlFor="recommend-false"
-                className={cn(
-                  'flex cursor-pointer items-center rounded-md border px-4 py-2 peer-data-[state=checked]:bg-primary peer-data-[state=checked]:text-primary-foreground',
-                  'hover:bg-muted',
-                )}
-              >
-                <ThumbsDown className="mr-2 size-5" />
-                추천하지 않음
-              </Label>
-            </div>
-          </RadioGroup>
+          <div className="flex gap-4">
+            <Button
+              ref={field.ref}
+              type="button"
+              variant={field.value === true ? 'default' : 'outline'}
+              onClick={() => field.onChange(true)}
+              className={cn(
+                'flex w-[100px] items-center justify-center',
+                isNotNil(errors.recommended) && 'border-red-500',
+              )}
+            >
+              <ThumbsUp className="mr-2 size-5" />
+              추천
+            </Button>
+            <Button
+              type="button"
+              variant={field.value === false ? 'default' : 'outline'}
+              onClick={() => field.onChange(false)}
+              className={cn(
+                'flex w-[100px] items-center justify-center',
+                isNotNil(errors.recommended) && 'border-red-500',
+              )}
+            >
+              <ThumbsDown className="mr-2 size-5" />
+              비추천
+            </Button>
+          </div>
         )}
       />
       {isNotNil(errors.recommended) && <p className="text-red-500">{errors.recommended.message}</p>}
